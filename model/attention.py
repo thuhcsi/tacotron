8ee774460290bedfae61c316ac26eb642fdcce59
zywu <johnson.tsing@gmail.com>
2021-03-17 19:30:18 +0800

    Add LSA, change the attention interface for overriding

--- model/attention.py
+++ model/attention.py
@@ -1,35 +1,119 @@
 """ adapted from https://github.com/r9y9/tacotron_pytorch """
+""" LSA from https://github.com/NVIDIA/tacotron2 """
 
 import torch
 from torch import nn
 
 
 class BahdanauAttention(nn.Module):
-    def __init__(self, query_dim, attn_dim):
+    """
+    BahdanauAttention
+
+    This attention is described in:
+        D. Bahdanau, K. Cho, and Y. Bengio, "Neural Machine Translation by Jointly Learning to Align and Translate,"
+        in International Conference on Learning Representation (ICLR), 2015.
+        https://arxiv.org/abs/1409.0473
+    """
+    def __init__(self, query_dim, attn_dim, score_mask_value=-float("inf")):
         super(BahdanauAttention, self).__init__()
-        # query layer to project query to hidden representation
+
+        # Query layer to project query to hidden representation
         # (query_dim -> attn_dim)
         self.query_layer = nn.Linear(query_dim, attn_dim, bias=False)
+
+        # For computing alignment energies
         self.tanh = nn.Tanh()
         self.v = nn.Linear(attn_dim, 1, bias=False)
 
-    def forward(self, query, processed_memory):
+        # For computing weights
+        self.score_mask_value = score_mask_value
+
+    def forward(self, query, processed_memory, mask=None):
         """
+        Get normalized attention weight
+
         Args:
             query: (batch, 1, dim) or (batch, dim)
             processed_memory: (batch, max_time, dim)
+            mask: (batch, max_time)
         """
         if query.dim() == 2:
             # insert time-axis for broadcasting
             query = query.unsqueeze(1)
-        # (batch, 1, dim)
+
+        # Alignment energies
+        alignment = self.get_alignment_energies(query, processed_memory)
+
+        if mask is not None:
+            mask = mask.view(query.size(0), -1)
+            alignment.data.masked_fill_(mask, self.score_mask_value)
+
+        # Normalize attention weight
+        alignment = nn.Softmax(dim=1)(alignment)
+
+        # (batch, max_time)
+        return alignment
+
+    def get_alignment_energies(self, query, processed_memory):
+        """
+        Compute the alignment energies
+        """
+        # Query (batch, 1, dim)
         processed_query = self.query_layer(query)
 
-        # (batch, max_time, 1)
+        # Alignment energies (batch, max_time, 1)
         alignment = self.v(self.tanh(processed_query + processed_memory))
 
         # (batch, max_time)
         return alignment.squeeze(-1)
+
+
+class LocationSensitiveAttention(BahdanauAttention):
+    """
+    LocationSensitiveAttention (LSA)
+
+    This attention is described in:
+        J. K. Chorowski, D. Bahdanau, D. Serdyuk, K. Cho, and Y. Bengio, "Attention-based Models for Speech Recognition,"
+        in Advances in Neural Information Processing Systems, 2015, pp. 577-585.
+        https://arxiv.org/abs/1506.07503
+    """
+    def __init__(self, query_dim, attn_dim, filters=32, kernel_size=31, score_mask_value=-float("inf")):
+        super(LocationSensitiveAttention, self).__init__(query_dim, attn_dim, score_mask_value)
+
+        # Location layer: Conv1d followd by Linear
+        self.conv = nn.Conv1d(1, filters, padding=(kernel_size - 1) // 2, kernel_size=kernel_size, bias=True)
+        self.L = nn.Linear(filters, attn_dim, bias=False)
+
+        # Cumulative attentions
+        self.cumulative = None
+
+    def init_attention(self, processed_memory):
+        b, t, c = processed_memory.size()
+        self.cumulative = processed_memory.data.new(b, t).zero_()
+
+    def get_alignment_energies(self, query, processed_memory):
+        # Query (batch, 1, dim)
+        processed_query = self.query_layer(query)
+
+        # Location feature
+        location = self.cumulative.unsqueeze(1)
+        processed_loc = self.L(self.conv(location).transpose(1, 2))
+
+        # Alignment energies (batch, max_time, 1)
+        alignment = self.v(self.tanh(processed_query + processed_memory + processed_loc))
+
+        # (batch, max_time)
+        return alignment.squeeze(-1)
+        
+    def forward(self, query, processed_memory, mask=None):
+        # Get normalized attention weight
+        alignment = super(LocationSensitiveAttention, self).forward(query, processed_memory, mask)
+
+        # Cumulative attentions
+        self.cumulative = self.cumulative + alignment
+
+        # (batch, max_time)
+        return alignment
 
 
 def get_mask_from_lengths(memory, memory_lengths):
@@ -46,12 +130,10 @@ def get_mask_from_lengths(memory, memory_lengths):
 
 
 class AttentionWrapper(nn.Module):
-    def __init__(self, rnn_cell, attention_mechanism,
-                 score_mask_value=-float("inf")):
+    def __init__(self, rnn_cell, attention_mechanism):
         super(AttentionWrapper, self).__init__()
         self.rnn_cell = rnn_cell
         self.attention_mechanism = attention_mechanism
-        self.score_mask_value = score_mask_value
 
     def forward(self, query, attention, cell_state, memory,
                 processed_memory=None, mask=None, memory_lengths=None):
@@ -72,16 +154,9 @@ class AttentionWrapper(nn.Module):
         else:
             query = cell_output
 
-        # Alignment
+        # Normalized attention weight
         # (batch, max_time)
-        alignment = self.attention_mechanism(query, processed_memory)
-
-        if mask is not None:
-            mask = mask.view(query.size(0), -1)
-            alignment.data.masked_fill_(mask, self.score_mask_value)
-
-        # Normalize attention weight
-        alignment = nn.Softmax(dim=1)(alignment)
+        alignment = self.attention_mechanism(query, processed_memory, mask)
 
         # Attention context vector
         # (batch, 1, dim)
